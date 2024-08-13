7fbed8ed3c0e9b0b1776039093caeab01a99b52e
Sanjay Kumar Srikakulam <sanjaysrikakulam@gmail.com>
2024-08-13 14:11:26 +0000

    add changes after domain migration

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,7 +5,7 @@ variable "zone_usegalaxy_esg" {
 resource "aws_route53_record" "galaxy-instance" {
   allow_overwrite = true
   zone_id         = var.zone_usegalaxy_esg
-  name            = "galaxyesginstance.esgwps.uno"
+  name            = "galaxyesginstance.esgwps.lol"
   type            = "A"
   ttl             = "7200"
   records         = [openstack_compute_instance_v2.galaxy_instance.access_ip_v4]
@@ -14,7 +14,7 @@ resource "aws_route53_record" "galaxy-instance" {
 resource "aws_route53_record" "usegalaxy-esg" {
   allow_overwrite = true
   zone_id         = var.zone_usegalaxy_esg
-  name            = "usegalaxy.esgwps.uno"
+  name            = "usegalaxy.esgwps.lol"
   type            = "CNAME"
   ttl             = "7200"
   records         = [openstack_compute_instance_v2.galaxy_instance.access_ip_v4]
@@ -23,7 +23,7 @@ resource "aws_route53_record" "usegalaxy-esg" {
 resource "aws_route53_record" "galaxy-esg-mq" {
   allow_overwrite = true
   zone_id         = var.zone_usegalaxy_esg
-  name            = "mqesginstance.esgwps.uno"
+  name            = "mqesginstance.esgwps.lol"
   type            = "A"
   ttl             = "7200"
   records         = [openstack_compute_instance_v2.mq_instance.access_ip_v4]
@@ -32,7 +32,7 @@ resource "aws_route53_record" "galaxy-esg-mq" {
 resource "aws_route53_record" "galaxy-esg-rustus" {
   allow_overwrite = true
   zone_id         = var.zone_usegalaxy_esg
-  name            = "uploadesginstance.esgwps.uno"
+  name            = "uploadesginstance.esgwps.lol"
   type            = "A"
   ttl             = "7200"
   records         = [openstack_compute_instance_v2.rustus_instance.access_ip_v4]
